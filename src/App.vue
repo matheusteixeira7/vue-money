a4453e4830afd2744c4bcd46ccfd0e60ede7292e
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-15 22:11:06 -0300

    feat: add dynamically main card values

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,146 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes.light.background }">
     <v-main>
-      <DashboardHeaderVue />
+      <DashboardHeader />
       <div class="main-cards">
         <div class="d-flex overflow-x-auto">
-          <MainCardVue
+          <MainCard
             name="Entradas"
-            amount="R$ 17.400,00"
+            :amount="
+              transactions.reduce(
+                (acc, transaction) =>
+                  transaction.value > 0 ? acc + transaction.value : acc,
+                0
+              )
+            "
             last-transaction="13 de abril"
             transaction-type="income"
           />
-          <MainCardVue
+          <MainCard
             name="Saídas"
-            amount="R$ 15.400,00"
+            :amount="
+              transactions.reduce(
+                (acc, transaction) =>
+                  transaction.value < 0 ? acc + transaction.value : acc,
+                0
+              )
+            "
             last-transaction="12 de abril"
             transaction-type="outcome"
           />
-          <MainCardVue
+          <MainCard
             name="Total"
-            amount="R$ 17.400,00"
+            :amount="
+              transactions.reduce((total, transaction) => {
+                return total + transaction.value;
+              }, 0)
+            "
             last-transaction="13 de abril"
             transaction-type=""
           />
         </div>
-        <TransactionsListVue />
+
+        <div class="pa-6 d-flex justify-space-between">
+          <span>Listagem</span>
+          <span>4 itens</span>
+        </div>
+
+        <TransactionsList
+          v-for="transaction in transactions"
+          :key="transaction.id"
+          :title="transaction.title"
+          :value="transaction.value"
+          :type="transaction.type"
+          :date="transaction.date"
+        />
       </div>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import DashboardHeaderVue from './components/dashboard/DashboardHeader.vue';
-import MainCardVue from './components/dashboard/MainCard.vue';
-import TransactionsListVue from './components/dashboard/TransactionsList.vue';
+import { defineComponent } from "@vue/composition-api";
 
-export default {
+import DashboardHeader from "./components/dashboard/DashboardHeader.vue";
+import MainCard from "./components/dashboard/MainCard.vue";
+import TransactionsList from "./components/dashboard/TransactionsList.vue";
+
+export default defineComponent({
   components: {
-    MainCardVue,
-    DashboardHeaderVue,
-    TransactionsListVue,
+    MainCard,
+    DashboardHeader,
+    TransactionsList,
   },
-};
+  data() {
+    return {
+      transactions: [
+        {
+          id: 1,
+          title: "Desenvolvimento de site",
+          value: 13000,
+          date: new Date().toLocaleDateString("pt-BR", {
+            year: "2-digit",
+            month: "numeric",
+            day: "numeric",
+          }),
+          type: "Trabalho",
+        },
+        {
+          id: 2,
+          title: "Supermercado",
+          value: -2000,
+          date: new Date().toLocaleDateString("pt-BR", {
+            year: "2-digit",
+            month: "numeric",
+            day: "numeric",
+          }),
+          type: "Casa",
+        },
+        {
+          id: 3,
+          title: "Prestação carro",
+          value: -3000,
+          date: new Date().toLocaleDateString("pt-BR", {
+            year: "2-digit",
+            month: "numeric",
+            day: "numeric",
+          }),
+          type: "Automóvel",
+        },
+        {
+          id: 4,
+          title: "Rendimento de investimentos",
+          value: 8000,
+          date: new Date().toLocaleDateString("pt-BR", {
+            year: "2-digit",
+            month: "numeric",
+            day: "numeric",
+          }),
+          type: "Investimentos",
+        },
+      ],
+    };
+  },
+  // methods: {
+  //   calculateIncome() {
+  //     return this.transactions.reduce(
+  //       (acc, transaction) => (transaction.value > 0 ? acc + transaction.value : acc),
+  //       0
+  //     );
+  //   },
+  //   calculateExpenses() {
+  //     return this.transactions.reduce(
+  //       (acc, transaction) => (transaction.value < 0 ? acc + transaction.value : acc),
+  //       0
+  //     );
+  //   },
+  //   calculateTotal() {
+  //     return this.transactions.reduce((total, transaction) => {
+  //       return total + transaction.value;
+  //     }, 0);
+  //   },
+  // },
+});
 </script>
 
 <style scoped>
--- src/components/dashboard/MainCard.vue
+++ src/components/dashboard/MainCard.vue
@@ -7,10 +7,7 @@
       class="pa-6"
     >
       <header class="d-flex justify-space-between mb-6">
-        <span
-          class="font-weight-regular text-body-2"
-          :style="{ color: changeTextColor }"
-        >
+        <span class="font-weight-regular text-body-2" :style="{ color: changeTextColor }">
           {{ name }}
         </span>
         <div>
@@ -19,11 +16,13 @@
       </header>
 
       <main class="d-flex flex-column">
-        <span
-          class="font-weight-medium amount"
-          :style="{ color: changeTextColor }"
-        >
-          {{ amount }}
+        <span class="font-weight-medium amount" :style="{ color: changeTextColor }">
+          {{
+            amount.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })
+          }}
         </span>
         <span
           class="font-weight-regular last-transaction"
@@ -37,7 +36,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent } from "@vue/composition-api";
 export default defineComponent({
   props: {
     name: {
@@ -45,7 +44,7 @@ export default defineComponent({
       required: true,
     },
     amount: {
-      type: String,
+      type: Number,
       required: true,
     },
     lastTransaction: {
@@ -62,45 +61,45 @@ export default defineComponent({
   },
   computed: {
     getColor() {
-      if (this.transactionType === 'income') {
-        return 'secondary';
-      } else if (this.transactionType === 'outcome') {
-        return 'error';
+      if (this.transactionType === "income") {
+        return "secondary";
+      } else if (this.transactionType === "outcome") {
+        return "error";
       } else {
-        return 'white';
+        return "white";
       }
     },
     getIcon() {
-      if (this.transactionType === 'income') {
-        return 'mdi-arrow-up-circle-outline';
-      } else if (this.transactionType === 'outcome') {
-        return 'mdi-arrow-down-circle-outline';
+      if (this.transactionType === "income") {
+        return "mdi-arrow-up-circle-outline";
+      } else if (this.transactionType === "outcome") {
+        return "mdi-arrow-down-circle-outline";
       } else {
-        return 'mdi-currency-usd';
+        return "mdi-currency-usd";
       }
     },
     changeCardBgColor() {
-      if (this.transactionType === 'income') {
+      if (this.transactionType === "income") {
         return this.$vuetify.theme.themes.light.white;
-      } else if (this.transactionType === 'outcome') {
+      } else if (this.transactionType === "outcome") {
         return this.$vuetify.theme.themes.light.white;
       } else {
         return this.$vuetify.theme.themes.light.secondary;
       }
     },
     changeTextColor() {
-      if (this.transactionType === 'income') {
+      if (this.transactionType === "income") {
         return this.$vuetify.theme.themes.light.titles;
-      } else if (this.transactionType === 'outcome') {
+      } else if (this.transactionType === "outcome") {
         return this.$vuetify.theme.themes.light.titles;
       } else {
         return this.$vuetify.theme.themes.light.white;
       }
     },
     changeLastTransactionTextColor() {
-      if (this.transactionType === 'income') {
+      if (this.transactionType === "income") {
         return this.$vuetify.theme.themes.light.text;
-      } else if (this.transactionType === 'outcome') {
+      } else if (this.transactionType === "outcome") {
         return this.$vuetify.theme.themes.light.text;
       } else {
         return this.$vuetify.theme.themes.light.white;
